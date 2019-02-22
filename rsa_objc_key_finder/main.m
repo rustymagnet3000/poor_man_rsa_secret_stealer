1d7c2494cfa27ef6226e49c826686bc84dcb11c1
rusty_magneto <rusty_magneto@rustymagnet3000.local>
2019-02-22 06:10:35 +0000

    Initial Commit

---
+++ rsa_objc_key_finder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  rsa_objc_key_finder
+//
+//  Created by rusty_magneto on 22/02/2019.
+//  Copyright © 2019 rusty_magneto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
