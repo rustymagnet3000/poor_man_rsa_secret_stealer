a6eb6994cb9705c49dcc9d8aa3fcfbfe355d6ff9
Rusty Robot <rusty.robot@local>
2019-03-05 10:55:41 +0000

    gmp api for determining whether a large number was prime

--- rsa_objc_key_finder/main.m
+++ rsa_objc_key_finder/main.m
@@ -6,22 +6,41 @@ int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
         
-        printf("[+]Started\n");
-       // __unused YDFindFactors *find = [[YDFindFactors alloc] init];
-       // __unused YDRunLoop *rl = [[YDRunLoop alloc] init];
-    
+        printf ("[+]N passed to program: %s \n", argv[1]);
+        
         if (argc <= 1){
             printf ("Usage: %s <number> \n", argv[0]);
             return 2;
         }
 
+        mpz_t n;
+        int flag;
+        int reps = 15;
 
+        mpz_init ( n );
 
+        flag = mpz_set_str(n,argv[1], 10);
+        assert (flag == 0);
+
+        reps = mpz_probab_prime_p ( n, reps );
+
+        switch (reps) {
+            case 0:
+                puts("Non-Prime");
+                break;
+            case 1:
+                puts("Prime (probably)");
+                break;
+            case 2:
+                puts("Prime");
+                break;
+            default:
+                puts("Unhandled");
+                break;
+        }
+
+        mpz_clear ( n );
         
     }
     return 0;
 }
-
-
-
-
