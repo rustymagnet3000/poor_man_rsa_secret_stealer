9ba1a810132ec70115d825d05beb6490fa21cef2
Rusty Robot <rustyrobot@robot.com>
2020-04-06 12:20:17 +0100

    Decrypt working as expecting with GMP.  The function that I missed was mpz_pown for doing the decrypt step. No requirement to move to OpenSSL.

--- objc_playground/main.m
+++ objc_playground/main.m
@@ -9,73 +9,80 @@
 @end
 
 @interface FooBar : NSObject <YDDeriveDecryptionKey>{
-    NSMutableArray *_foundFactors;
-    unsigned long long _PHI;
-    NSNumber *_exponent;
-    NSNumber *_p;
-    NSNumber *_q;
+    mpz_t   _exponent,
+            _n,
+            _p,
+            _q,
+            _PHI,
+            _derivedDecryptionKey,
+            _cipherText,
+            _plainText;
 }
 
-- (unsigned long long)getPHI;
 @end
 
 @implementation FooBar
 
-- (unsigned long long)getPHI{
-    return _PHI;
-}
-
 - (instancetype)init{
     self = [super init];
     if (self) {
-        _exponent = [NSNumber numberWithUnsignedLongLong:65537];
-        _PHI = 5;
-        _p = [NSNumber numberWithUnsignedLongLong:21277];
-        _q = [NSNumber numberWithUnsignedLongLong:1180873];
-        _foundFactors = [NSMutableArray arrayWithObjects:_p, _q, nil];
+        int flag = 0;
+        mpz_inits ( _exponent, _n, _p, _q, _PHI,_derivedDecryptionKey, _cipherText, _plainText, NULL);
+        
+        NSString *e = @"65537";
+        flag = mpz_set_str(_exponent,[e UTF8String], 10);
+        assert (flag == 0);
+
+        NSString *n = @"1034776851837418228051242693253376923";
+        flag = mpz_set_str(_n,[n UTF8String], 10);
+        assert (flag == 0);
+
+        NSString *p = @"1086027579223696553";
+        flag = mpz_set_str(_p,[p UTF8String], 10);
+        assert (flag == 0);
+        
+        NSString *q = @"952809000096560291";
+        flag = mpz_set_str(_q,[q UTF8String], 10);
+        assert (flag == 0);
+
+        NSString *cipherText = @"582984697800119976959378162843817868";
+        flag = mpz_set_str(_cipherText,[cipherText UTF8String], 10);
+        assert (flag == 0);
     }
   return self;
 }
 
 -(BOOL)totient{
     
-    NSUInteger factorCount = [_foundFactors count];
-    if (factorCount != 2){
-        return NO;
-    }
-    _PHI = ([_p unsignedLongLongValue] - 1) * ([_q unsignedLongLongValue] - 1);
+    mpz_t tempP, tempQ;
+    mpz_inits ( tempP, tempQ, NULL);
+    
+    mpz_sub_ui(tempP,_p,1);
+    mpz_sub_ui(tempQ,_q,1);
+    gmp_printf("[+]\tNew p: %Zd: Old p:%Zd\n", tempP, _p);
+    gmp_printf("[+]\tNew q: %Zd: Old q:%Zd\n", tempQ, _q);
+    
+    mpz_mul(_PHI,tempP,tempQ);
+    gmp_printf("[+]\tPHI:%Zd\n", _PHI);
+    
+    mpz_clears ( tempP, tempQ, NULL );
     return YES;
 }
 
+-(void)decryptMessage{
+    mpz_powm(_plainText, _cipherText, _derivedDecryptionKey, _n);
+    gmp_printf("[+]\tplainText:%Zd\n", _plainText);
+}
+
+
+
 -(BOOL)deriveMultiplicativeInverse{
     
-    mpz_t phi, e, actualDecKey, derivedDecKey;
-    mpz_inits ( phi, e, actualDecKey, derivedDecKey, NULL);
     int flag = 0;
     
-    const char *rawPhi = "1034776851837418226012406113933120080";
-    const char *correctDecKey =  "568411228254986589811047501435713";
-    flag = mpz_set_str(phi,rawPhi, 10);
-    assert (flag == 0); // If zero, it succeeded.
-    flag = mpz_set_str(actualDecKey,correctDecKey, 10);
-    assert (flag == 0); // If zero, it succeeded.
-    
-    mpz_set_ui(e, [_exponent unsignedLongLongValue]);
-    gmp_printf("[+]\tphi ( in gmp ): %Zd: exponent:\t%Zd\n", phi, e);
-    
-    flag = 0;
-    flag = mpz_invert(derivedDecKey, e, phi);
+    flag = mpz_invert(_derivedDecryptionKey, _exponent, _PHI);
     assert (flag != 0);  // If inverse exists, the return value is non-zero
-
-    gmp_printf("[+]\tdecKey\t%Zd\n", derivedDecKey);
-
-    flag = mpz_cmp(derivedDecKey, actualDecKey);
-    if (flag == 0){
-        puts("Decryption key passed");
-    }
-    
-    mpz_clears ( phi, e, derivedDecKey, actualDecKey, NULL );
-    
+    gmp_printf("[+]\tdecKey\t%Zd\n", _derivedDecryptionKey);
     return YES;
 }
 
@@ -90,7 +97,7 @@ int main(int argc, const char * argv[]) {
             return EXIT_FAILURE;
         if([foo deriveMultiplicativeInverse] == NO)
             return EXIT_FAILURE;
-
+        [foo decryptMessage];
     }
     return 0;
 }
