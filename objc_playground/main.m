44ad6285b7014f9c5bcfed9ce23ffdad8221903d
Rusty Robot <rustyrobot@robot.com>
2020-06-30 05:40:15 +0100

    Added a TOC to Readme. Bug still present

--- objc_playground/main.m
+++ objc_playground/main.m
@@ -3,24 +3,48 @@
 
 #define N "10403" // (101 * 103)
 
+/*
+ 
+ #define N "10403" // (101 * 103)
+ x = (x * x + 1) % number;
+ 
+ This MUST output:
+ 
+ x = 2
+ x = 5
+ x = 26
+ x = 677
+ x = 598
+ x = 3903
+ x = 3418
+ x = 156
+ x = 3531
+ x = 5168
+ x = 3724
+ x = 978
+ x = 9812
+ x = 5983
+ x = 9970
+ 
+ */
+
 int main(void)
 {
-    mpz_t exp, n, x;
-
-    int flag;
+    mpz_t exp, n, x, x_increment_1;
+    int flag, i = 0;
     
-    mpz_inits(n, NULL);
+    mpz_inits(n, x_increment_1, NULL);
     mpz_init_set_ui(x, 2);
 
     flag = mpz_set_str(n, N, 10);
     assert (flag == 0);
     
-    int i = 0;
     do {
-        // x = (x * x + 1) % number;
         gmp_printf("X = %Zd\n", x);
-        mpz_add_ui(exp,x,1);
-        mpz_powm(x, x, exp, n);
+        mpz_add_ui(x_increment_1,x,1);
+        mpz_mul(x,x,x_increment_1);
+        mpz_mod( x, x, n );
+        
         i++;
     } while (i < 15 );
     
